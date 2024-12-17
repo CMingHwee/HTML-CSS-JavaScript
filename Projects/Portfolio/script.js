287ba34c7c26b44aef801ba267a18958c4d3504d
CMingHwee <chuaminghweesg@gmail.com>
2024-12-17 13:59:25 +0800

    Fixed smooth scrolling and added sticky navbar code

--- Projects/Portfolio/script.js
+++ Projects/Portfolio/script.js
@@ -1,3 +1,15 @@
-html {
-  scroll-behavior: smooth;
+document.documentElement.style.scrollBehavior = 'smooth';
+
+window.onscroll = function () { stickyNav() }; //triggers when scrolling
+
+var navbar = document.querySelector('header');
+var sticky = navbar.offsetTop; //diatance between top of page to header
+
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } //if scroll past this point, add sticky class to header
+else {
+ navbar.classList.remove("sticky");
+  } //when scroll back up, remove sticky class from header
 }
